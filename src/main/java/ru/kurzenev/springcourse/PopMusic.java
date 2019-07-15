fcf8f6e625f1bb431669fa37e03b9223d22eb081
Роман Курзенев <roman.kurzenev@gmail.com>
2019-07-15 20:29:01 +0300

    Created new class PopMusic and RockMusic

---
+++ src/main/java/ru/kurzenev/springcourse/PopMusic.java
@@ -0,0 +1,10 @@
+package ru.kurzenev.springcourse;
+
+public class PopMusic implements Music{
+
+    @Override
+    public String getSong(){
+        return "Oops!...i did it again";
+    }
+
+}
---
+++ src/main/java/ru/kurzenev/springcourse/RockMusic.java
@@ -0,0 +1,8 @@
+package ru.kurzenev.springcourse;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Master of puppets";
+    }
+}
